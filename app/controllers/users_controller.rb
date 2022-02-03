4f812971a39f211c1d6322c414f4fe3552423932
Lance Hebert <LSUHEBERT@gmail.com>
2022-02-02 16:38:51 -0800

    commit tree 23e8117283d1c30d6c5a0a9d62e7e5b9910ce27b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+def show
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user, include: :items
+end
+
+private
+
+    def user_params
+        params.permit(:username,:city)
+
+    end
+
 end
